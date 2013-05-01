6f229cd5dee64029a8b7d516ea144396bd329c54
zfei <feizhiye@yahoo.com>
2013-05-01 18:16:11 -0500

    Get single real name function

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 __author__ = 'zfei'
 
+import urllib2
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+def pull(screen_name):
+    page = urllib2.urlopen("https://twitter.com/" + screen_name)
+    soup = BeautifulSoup(page)
+    print soup.select("span.profile-field")[0].string
+
+def update_each_user(collection):
+
+
+def main():
+    db = MongoClient().tweet_data
+    collection = db.tweet_data
+    cursor = collection.find(tailable=True)
+
+main()
